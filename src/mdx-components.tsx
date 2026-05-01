048d818b548c3249ab4b39f37788f4988795b092
Chunyu <2822603942@qq.com>
2026-05-02 02:27:12 +0800

    没啥用

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -6,15 +6,7 @@ import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: ({ src, alt, width, height, ...rest }) => (
-      <ImageZoom
-        src={src as string}
-        alt={alt as string}
-        width={width as number | undefined}
-        height={height as number | undefined}
-        {...(rest as any)}
-      />
-    ),
+    img: (props) => <ImageZoom {...(props as any)} />,
     pre: ({ ref: _ref, ...props }) => (
       <CodeBlock {...props}>
         <Pre>{props.children}</Pre> 

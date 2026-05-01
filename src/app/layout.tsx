c1aac01a4764804d756ae9f016071559d980e0a9
Chunyu <2822603942@qq.com>
2026-05-01 18:04:16 +0800

    修改字体使用，修改网站元信息

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,19 @@ const mono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const metadata = {
+  title: {
+    default: "AstroBox 文档",
+    template: "%s | AstroBox 文档",
+  },
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="zh-CN" className={`${geist.variable} ${mono.variable}`} suppressHydrationWarning>
+    <html lang="zh-CN" className={`${geist.variable} ${mono.variable} font-setting`} suppressHydrationWarning>
+      <head>
+        <link rel="stylesheet" href="https://i02.appmifile.com/i18n/fonts/MiSansChinese/index.css" />
+      </head>
       <body className="relative flex min-h-screen flex-col" suppressHydrationWarning>
         <RootProvider>{children}</RootProvider>
         <Footer />
